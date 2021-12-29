9149036700899f151f83118adea7bfaaba08e5a5
saurav-khantwal <souravkhantwal100@gmail.com>
2021-12-29 23:49:35 +0530

    Corrections

--- MAIN_APP/__init__.py
+++ MAIN_APP/__init__.py
@@ -1,14 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from MAIN_APP import routes
 
 
 app = Flask(__name__)
 
 ENV = 'PROD'
 
-if ENV == 'DEV':
+if ENV == 'dev':
     app.debug = True
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:prototype@localhost/TODO'
 
@@ -24,4 +23,5 @@ db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login_page'
 login_manager.login_message_category = 'info'
+from MAIN_APP import routes
 
